a9823f8d965f961044e8967636f4e299865574be
Irem Paul Chinonso <chinonsopaul@gmail.com>
2024-08-26 08:26:27 +0100

    Time: 30 ms (5.11%), Space: 26 MB (22.1%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,33 +3,27 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         //using brute force with hashmaps
         map<string, int> validMap; 
+        map<string, int> validMap1;  
         for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){
+            for(int j = 0; j < board[i].size(); j++){ 
                 char curr = board[i][j];
+                char curr1 = board[j][i];
+                //3x3 sub matrix wise label
                 string key = curr + to_string((int)floor(i/3))
                 + to_string((int)floor(j/3));
+                //row-wise label
                 string key1 = curr + to_string(i);
+                //column-wise label
+                string key2 = curr1 + to_string(i);
                 if(curr != '.'){
                     validMap[key]++;
                     validMap[key1]++;
-                } 
-                if(curr != '.' && (validMap[key] > 1||validMap[key1]>1)){
-                    return false;
                 }
-            }
-        }
-        map<string, int> validMap1;
-        for(int j = 0; j < board[0].size(); j++){
-            for(int i = 0; i < board.size(); i++){
-                char curr = board[i][j];
-                string key = curr + to_string((int)floor(i/3))
-                + to_string((int)floor(j/3));
-                string key1 = curr + to_string(j);
-                if(curr != '.'){
-                    validMap1[key]++;
-                    validMap1[key1]++;
-                }
-                if(curr != '.' && (validMap1[key] > 1||validMap1[key1]>1)){
+                if(curr1 != '.') validMap1[key2]++;
+                if(
+                    (curr != '.' && (validMap[key] > 1||validMap[key1]>1))
+                    || (curr1 != '.' && validMap1[key2]>1)
+                ){
                     return false;
                 }
             }
