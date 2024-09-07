8b0890ea677259aa0ada902534cf9cd93c866f1f
Irem Paul Chinonso <chinonsopaul@gmail.com>
2024-09-07 02:42:42 +0100

    Time: 19 ms (45.73%), Space: 24.1 MB (55.6%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        //using brute force with hashmaps
-        map<string, int> validMap; 
-        map<string, int> validMap1;  
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){ 
-                char curr = board[i][j];
-                char curr1 = board[j][i];
-                //3x3 sub matrix wise label
-                string key = curr + to_string((int)floor(i/3))
-                + to_string((int)floor(j/3));
-                //row-wise label
-                string key1 = curr + to_string(i);
-                //column-wise label
-                string key2 = curr1 + to_string(i);
-                if(curr != '.'){
-                    validMap[key]++;
-                    validMap[key1]++;
-                }
-                if(curr1 != '.') validMap1[key2]++;
-                if(
-                    (curr != '.' && (validMap[key] > 1||validMap[key1]>1))
-                    || (curr1 != '.' && validMap1[key2]>1)
-                ){
+        vector<vector<bool>> rows(9, vector<bool>(9,false)
+        );
+        vector<vector<bool>> cols(9, vector<bool>(9, false));
+        vector<vector<bool>> subgrid(9, vector<bool>(9, false));
+        for(int row = 0; row < 9; row++){
+            for(int col = 0; col < 9; col++){
+                char currChar = board[row][col];
+                if(currChar == '.') continue;
+
+                int num = currChar - '0' - 1;
+                int subgridIdx = (row/3)*3 + col/3;
+
+                if(cols[col][num] || rows[row][num] 
+                || subgrid[subgridIdx][num]){
                     return false;
                 }
+
+                cols[col][num] = true;
+                rows[row][num] = true;
+                subgrid[subgridIdx][num] = true;
             }
         }
         return true;
